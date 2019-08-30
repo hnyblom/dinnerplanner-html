74aacf7ae6d15a56de7bd41fe619749ca358039f
Timothy Ko <timothy.l.ko@gmail.com>
2019-08-30 15:57:19 +0200

    changes

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -1,3 +1,5 @@
+const BASE_URL = "http://sunset.nada.kth.se:8080/iprog/group/5";
+
 //DinnerModel Object constructor
 class DinnerModel {
   constructor() {
@@ -5,14 +7,8 @@ class DinnerModel {
     this.nrGuests = 0;
     this.guests = guestsConst;
     this.menu = [];
-
     //TODO Lab 1 implement the data structure that will hold number of guest
     // and selected dishes for the dinner menu
-  }
-
-  test() {
-    this.getTotalMenuPrice();
-    var nothing = 0;
   }
 
   setNumberOfGuests(num) {
@@ -34,8 +30,7 @@ class DinnerModel {
 
   //Returns all the dishes on the menu.
   getFullMenu() {
-    var allDishes = this.getAllDishes(this.menu);
-    return allDishes;
+    return this.menu;
   }
 
   //Returns all ingredients for all the dishes on the menu.
@@ -61,17 +56,30 @@ class DinnerModel {
   //it is removed from the menu and the new one added.
   addDishToMenu(id) {
     //Get the wanted dish from the database of dishes
-    var lookupDish = this.getDish(this.dishes, id);
-    var type = lookupDish.type;
-    var existingDish = this.getSelectedDish(type);
+    this.getDish(id).then(lookupDish => {
+      var lookupDish = this.dishes.find(item => item.id === id); //this.getDish(this.dishes, id);
+      var type = lookupDish.type;
+      var existingDish = this.getSelectedDish(type);
 
-    if (existingDish) {
-      this.removeDishFromMenu(existingDish.id);
-    }
+      if (existingDish) {
+        this.removeDishFromMenu(existingDish.id);
+      }
 
-    //Add the dish to the menu
-    var dish = this.getDish(this.dishes, id);
-    this.menu.push(dish);
+      //Add the dish to the menu
+      var dish = lookupDish;
+      this.menu.push(dish);
+    });
+    // var lookupDish = this.dishes.find(item => item.id === id); //this.getDish(this.dishes, id);
+    // var type = lookupDish.type;
+    // var existingDish = this.getSelectedDish(type);
+
+    // if (existingDish) {
+    //   this.removeDishFromMenu(existingDish.id);
+    // }
+
+    // //Add the dish to the menu
+    // var dish = lookupDish;
+    // this.menu.push(dish);
   }
 
   //Removes dish from menu
@@ -87,28 +95,51 @@ class DinnerModel {
   //function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
   //you can use the filter argument to filter out the dish by name or ingredient (use for search)
   //if you don't pass any query all the dishes will be returned
-  getAllDishes(from, type, query) {
-    return from.filter(function(dish) {
-      let found = true;
-      if (query) {
-        found = false;
-        dish.ingredients.forEach(function(ingredient) {
-          if (ingredient.name.indexOf(query) !== -1) {
-            found = true;
-          }
-        });
-        if (dish.name.indexOf(query) !== -1) {
-          found = true;
+  getAllDishes(type, query) {
+    let searchParams = new URLSearchParams();
+    if (type && type !== undefined) {
+      searchParams.append("type", type);
+    }
+    if (query && query !== undefined) {
+      searchParams.append("query", query);
+    }
+
+    let searchStr = searchParams.toString();
+    return fetch(
+      `${BASE_URL}/recipes/search${searchStr === "" ? "" : "?"}${searchStr}`,
+      {
+        headers: {
+          "X-Mashape-Key": "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767"
         }
       }
-      if (type === undefined) {
-        return dish;
-      }
-      if (type == "") {
-        return found;
-      }
-      return dish.type === type && found;
-    });
+    )
+      .then(res => res.json())
+      .then(json => {
+        return json.results;
+      })
+      .catch(error => console.error("Error:", error));
+
+    // return from.filter(function (dish) {
+    //   let found = true;
+    //   if (query) {
+    //     found = false;
+    //     dish.ingredients.forEach(function (ingredient) {
+    //       if (ingredient.name.indexOf(query) !== -1) {
+    //         found = true;
+    //       }
+    //     });
+    //     if (dish.name.indexOf(query) !== -1) {
+    //       found = true;
+    //     }
+    //   }
+    //   if (type === undefined) {
+    //     return dish;
+    //   }
+    //   if (type == "") {
+    //     return found;
+    //   }
+    //   return dish.type === type && found;
+    // });
   }
 
   //Removes duplicates from lists
@@ -120,13 +151,22 @@ class DinnerModel {
   }
 
   //function that returns a dish of specific ID
-  getDish(from, id) {
-    for (let dsh of from) {
-      if (dsh.id == id) {
-        return dsh;
-      }
+  getDish(id) {
+    if (!id || id === "") {
+      return undefined;
     }
-    return undefined;
+    console.log(`${BASE_URL}/recipes/${id}/information`);
+    return fetch(`${BASE_URL}/recipes/${id}/information`, {
+      headers: {
+        "X-Mashape-Key": "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767"
+      }
+    }).then(res => res.json());
+    // for (let dsh of from) {
+    //   if (dsh.id == id) {
+    //     return dsh;
+    //   }
+    // }
+    // return undefined;
   }
 }
 
