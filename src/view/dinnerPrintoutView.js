58b0dfadeb2764d121d62299b7f91b951198d35f
Timothy Ko <timothy.l.ko@gmail.com>
2019-09-09 13:01:32 +0200

    update styling

--- src/view/dinnerPrintoutView.js
+++ src/view/dinnerPrintoutView.js
@@ -18,28 +18,30 @@ class DinnerPrintoutView {
       <div class="col">
         <hr />
       </div>
-      <div class="row">
-        <div class="col-2">
-          <img src="images/bakedbrie.jpg" class="card-img-top ml-2" alt="">
-        </div>
-        <div class="col-4">
-          <h3>Lasange</h3>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel laoreet orci. Nullam ut iaculis diam. Aliquam
-          magna nulla, congue ut elementum hendrerit, dignissim at mauris. Quisque ac felis sed nibh elementum euismod a sit amet
-          arcu. Maecenas a efficitur leo.
-          </p>
-        </div>
-        <div class="col-5">
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel laoreet orci. Nullam ut iaculis diam. Aliquam
-          magna nulla, congue ut elementum hendrerit, dignissim at mauris. Quisque ac felis sed nibh elementum euismod a sit amet
-          arcu. Maecenas a efficitur leo.
-          
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel laoreet orci. Nullam ut iaculis diam. Aliquam
-          magna nulla, congue ut elementum hendrerit, dignissim at mauris. Quisque ac felis sed nibh elementum euismod a sit amet
-          arcu. Maecenas a efficitur leo.
-          </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <img src="images/bakedbrie.jpg" class="img-fluid shadow" alt="">
+          </div>
+          <div class="col-4">
+            <h3>Lasange</h3>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel laoreet orci. Nullam ut iaculis diam. Aliquam
+            magna nulla, congue ut elementum hendrerit, dignissim at mauris. Quisque ac felis sed nibh elementum euismod a sit amet
+            arcu. Maecenas a efficitur leo.
+            </p>
+          </div>
+          <div class="col-4">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel laoreet orci. Nullam ut iaculis diam. Aliquam
+            magna nulla, congue ut elementum hendrerit, dignissim at mauris. Quisque ac felis sed nibh elementum euismod a sit amet
+            arcu. Maecenas a efficitur leo.
+            
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel laoreet orci. Nullam ut iaculis diam. Aliquam
+            magna nulla, congue ut elementum hendrerit, dignissim at mauris. Quisque ac felis sed nibh elementum euismod a sit amet
+            arcu. Maecenas a efficitur leo.
+            </p>
+          </div>
         </div>
       </div>
     `;
